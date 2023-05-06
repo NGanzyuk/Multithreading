5a63c4ddc5beacc5e803de869143cdb23570edb9
Ganzyuk Nikita <nganzyuk.edu@gamail.com>
2023-05-06 15:57:39 +0300

    "Add task"

--- parallel_collection/analyzerProgram/src/main/java/Main.java
+++ parallel_collection/analyzerProgram/src/main/java/Main.java
@@ -1,9 +1,14 @@
 import java.util.Queue;
 import java.util.Random;
 import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 
 
 public class Main {
+    public static BlockingQueue<String> qa = new ArrayBlockingQueue<>(100);
+    public static BlockingQueue<String> qb = new ArrayBlockingQueue<>(100);
+    public static BlockingQueue<String> qc = new ArrayBlockingQueue<>(100);
+    public static Thread generator;
     public static String generateText(String letters, int length) {
         Random random = new Random();
         StringBuilder text = new StringBuilder();
@@ -12,18 +17,56 @@ public class Main {
         }
         return text.toString();
     }
-    public static void main(String[] args) {
-        Thread thread1 = new Thread(() -> {
-            Queue queue = new ArrayBlockingQueue(100);
+
+
+    public static int textAnalyzer(BlockingQueue<String> queue, char letter){
+        int max = 0;
+        int counter = 0;
+        String str;
+        try {
+            while (generator.isAlive()){
+                str = queue.take();
+                for (char ch:str.toCharArray()) {
+                    if (ch == letter){
+                        counter++;
+                    }
+                }
+                if (counter > max){
+                    max = counter;
+                }
+                counter = 0;
+            }
+        }catch (InterruptedException e){
+            System.out.println(Thread.currentThread().getName()+" is interrupted");
+        }
+        return max;
+    }
+    public static Thread newThread(BlockingQueue<String> queue, char letter){
+        return new Thread(()->{
+            int max = textAnalyzer(queue, letter);
+            System.out.println("Максимальное количество символов "+letter+" равно " + max);
         });
-        Thread thread2 = new Thread(() -> {
-            Queue queue = new ArrayBlockingQueue(100);
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+         generator = new Thread(() ->{
+            for (int i = 0; i < 10_000; i++) {
+                String str = generateText("abc", 100_000);
+                qa.offer(str);
+                qb.offer(str);
+                qc.offer(str);
+            }
         });
-        Thread thread3 = new Thread(() -> {
-            Queue queue = new ArrayBlockingQueue(100);
-        });
-        Thread thread4 = new Thread(() -> {
-            Queue queue = new ArrayBlockingQueue(100);
-        });
+        generator.start();
+        Thread a = newThread(qa, 'a');
+        Thread b = newThread(qb, 'b');
+        Thread c = newThread(qc, 'c');
+        a.start();
+        b.start();
+        c.start();
+        a.join();
+        b.join();
+        c.join();
+
     }
 }
